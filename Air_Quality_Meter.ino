3a49652b830ab73b62b532afdef997ca47442eaa
KarloLeksic <lekxka@gmail.com>
2022-12-01 18:48:05 +0100

    Added functional code for a device

---
+++ Air_Quality_Meter.ino
@@ -0,0 +1,198 @@
+/*
+    The air quality sensor is a device that measures particulate concentration in the air and determines air quality.
+    It also measures temperature, humidity and pressure.
+
+    air-quality.h is a header file that contains parameters for device configuration and GUI class. You can configure
+    which sensors want to use by simply put 0 to the certain define. BME680 is used by default and you can't disable
+    them while CCS811 and PMS7003 are optional. You can also adjust refresh time, battery high and low levels values,
+    and temperature offset to calibrate the BME280 sensor.
+
+    gui.cpp contains all functions from the GUI class which is used to display measurements on the screen and initialize
+    sensors.
+
+    1 December 2022 by Soldered
+*/
+
+#include "src/air-quality.h"
+
+int page = 0, prevPage;            // Use those variables to select a page
+int currentStateClock;             // Store the status of the rotary encoder clock pin (HIGH or LOW)
+int lastStateClock;                // Store the PREVIOUS status of the rotary encoder clock pin (HIGH or LOW)
+unsigned long lastButtonPress = 0; // Use this to store if the rotaryEncoderBtn button was pressed or not
+
+int maxPage = 1;  // Max number of pages, 2 for the BME that it's always in use, later is added more if needed
+int pageOrder[5]; // The array which decides the order of pages we display
+
+unsigned long time1; // Time measurement for button click
+
+BME280 bme280;               // Define BME sensor object
+CCS_811 ccs811Sensor;        // Define CCS811 sensor object
+LCD lcd(20, 4);              // Define LCD object
+PMS7003 pms(PMS_RX, PMS_TX); // Define PMSx003, RX, TX
+
+GUI gui(&lcd, &bme280, &ccs811Sensor, &pms, &page, &maxPage);
+
+void setup()
+{
+    // Set analog reference to 1.1V to measure battery voltage
+    analogReference(INTERNAL);
+
+    // Turn the onboard LED off because we don't use it
+    pinMode(ONBOARD_LED, OUTPUT);
+    digitalWrite(ONBOARD_LED, LOW);
+
+    // Begin I2C comunication and set timeout
+    Wire.begin();
+    Wire.setWireTimeout(1E5);
+
+    // Initialize LCD and turn the backlight on
+    gui.displayBegin();
+
+    // Set encoder and button pins as inputs with pullups
+    pinMode(clock, INPUT_PULLUP);
+    pinMode(data, INPUT_PULLUP);
+    pinMode(rotaryEncoderBtn, INPUT_PULLUP);
+
+    // Read the initial state of clock pin of rotary encoder (it could be HIGH or LOW)
+    lastStateClock = digitalRead(clock);
+
+    // Initialize BME
+    gui.BMEbegin();
+
+    if (USE_CCS_SENSOR) // Initialize CCS811
+    {
+        gui.CCSbegin();
+        maxPage += 1; // Add 1 to max pages because the CCS needs 1 page
+    }
+
+    if (USE_PMS_SENSOR) // Initialize serial communication with PMS sensor
+    {
+        gui.PMSbegin();
+        maxPage += 3; // Add 3 to max pages because the PMS needs 3 page
+    }
+
+    // Depending on how many pages we have, we know which sensor is used and set pages
+    // Below are the indexes for each page:
+    // index 1 - BME first page (1/1)
+    // index 2 - CCS first page (1/1)
+    // index 3 - PMS first page (1/3)
+    // index 4 - PMS second page (2/3)
+    // index 5 - PMS third page (3/3)
+    switch (maxPage)
+    {
+    // Set order pages depending on which sensors using
+    case 1:               // Only BME is used
+        pageOrder[0] = 1; // BME first page
+        break;
+    case 2: // Using BME and CCS
+        pageOrder[0] = 1;
+        pageOrder[1] = 2;
+        break;
+    case 4: // Using BME and PMS
+        pageOrder[0] = 1;
+        pageOrder[1] = 3;
+        pageOrder[2] = 4; // PMS first page
+        pageOrder[3] = 5;
+        break;
+    case 5: // Using all 3 sensors
+        pageOrder[0] = 1;
+        pageOrder[1] = 2;
+        pageOrder[2] = 3;
+        pageOrder[3] = 4;
+        pageOrder[4] = 5;
+        break;
+    }
+
+    // Capture time for the values refresh
+    time1 = millis();
+
+    // Delay to see splash screen
+    delay(2000);
+
+    // Set default home page
+    gui.setPage(1);
+
+    // Set the previous page to know when the page is changed
+    prevPage = page;
+}
+
+void loop()
+{
+
+    // Read the current state of the rotary encoder CLK pin
+    currentStateClock = digitalRead(clock);
+
+    // If last and current state of the rotary encoder Clock are different, then "pulse occurred"
+    // React to only 1 state change to avoid double count
+    if (currentStateClock != lastStateClock && currentStateClock == 1)
+    {
+        // If the data state is different than the Clock state then
+        // the encoder is rotating "CCW" so we decrement
+        if (digitalRead(data) != currentStateClock)
+        {
+            page--;
+
+            // Prevent going to the negative numbers
+            if (page < 0)
+            {
+                page = maxPage - 1;
+            }
+        }
+        else
+        {
+            // Encoder is rotating CW so increment
+            page++;
+
+            // Prevent going to the more than 5 pages
+            if (page > maxPage - 1)
+            {
+                page = 0;
+            }
+        }
+    }
+
+    // We save last Clock state for next loop
+    lastStateClock = currentStateClock;
+
+    // Read the button state
+    int btnState = digitalRead(rotaryEncoderBtn);
+
+    // If we detect LOW signal, button is pressed
+    if (btnState == LOW)
+    {
+        // if 50ms have passed since last LOW pulse, it means that the
+        // button has been pressed, released and pressed again
+        if ((unsigned long)(millis() - lastButtonPress) > 50)
+        {
+            // If button is pressed, refresh measurements
+            gui.setPage(pageOrder[page]);
+            time1 = millis();
+        }
+
+        // Remember last button press event
+        lastButtonPress = millis();
+    }
+
+    // Put in a slight delay to help debounce the reading
+    delay(1);
+
+    // If the page is changing, change content
+    if (prevPage != page)
+    {
+        // setPage(pageOrder[page]);
+        gui.setPage(pageOrder[page]);
+        time1 = millis(); // The data is refreshed automatically when the new page is loaded so reset the timer in order
+                          // to have a refresh every 5 seconds
+        prevPage = page;
+    }
+
+    // Refresh sensor readings every 5 seconds without blocking the code
+    if ((unsigned long)(millis() - time1) > REFRESH_DATA_TIME)
+    {
+        // Set sensor readings depending on the page we are
+        gui.setPage(pageOrder[page]);
+
+        // Reset timer after refreshing
+        time1 = millis();
+    }
+}
---
+++ src/gui.cpp
@@ -0,0 +1,533 @@
+#include "air-quality.h"
+
+/**
+ * @brief           Constructor of the GUI class.
+ *
+ * @param *_lcd     Pointer to the LCD object used for display sensor values.
+ *
+ * @param *_bme280  Pointer to the BME280 sensor used for measure temperature, humidity and pressure.
+ * 
+ * @param *_ccs811Sensor Pointer to the CCS811 sensor used for measure Co2 and TVOC.
+ * 
+ * @param *_pms     Pointer to the PMS7003 sensor used for measuring particle concentration in the air.
+ * 
+ * @param *_page    Pointer to current page
+ * 
+ * @param *_maxPage Pointer to the variable of the max number of pages
+ */
+GUI::GUI(LCD *_lcd, BME280 *_bme280, CCS_811 *_ccs811Sensor, PMS7003 *_pms, int *_page, int *_maxPage)
+{
+    lcd = _lcd;
+    bme280 = _bme280;
+    ccs811Sensor = _ccs811Sensor;
+    pms = _pms;
+    page = _page;
+    maxPage = _maxPage;
+}
+
+/**
+ * @brief           Set the wanted page on the LCD, automatically read sensor data and print on it.
+ *
+ * @param page      The number of page we want to display.
+ *
+ * @return          None.
+ */
+void GUI::setPage(byte page)
+{
+    switch (page)
+    {
+    case 1:               // Page 1 is the page for the BME sensor
+        printBMEpage();   // Print BME page on the LCD
+        insertNumbers(1); // Print sensor measurements on the LCD
+        break;
+    case 2:               // Page 3 is the page for the CCS sensor
+        printCCSpage();   // Print CCS page on the LCD
+        insertNumbers(2); // Print sensor measurements on the LCD
+        break;
+    case 3:               // Page 4 is the first of three pages for the PMS sensor
+        printPMSpage(1);  // Print PMS page on the LCD
+        insertNumbers(3); // Print sensor measurements on the LCD
+        break;
+    case 4:               // Page 5 is the second of three pages for the PMS sensor
+        printPMSpage(2);  // Print PMS page on the LCD
+        insertNumbers(4); // Print sensor measurements on the LCD
+        break;
+    case 5:               // Page 6 is the third of three pages for the PMS sensor
+        printPMSpage(3);  // Print PMS page on the LCD
+        insertNumbers(5); // Print sensor measurements on the LCD
+        break;
+    }
+
+    insertPageNum();          // Print page number
+    printBatteryPercentage(); // Print battery percentage
+}
+
+/**
+ * @brief           Display content of the BME page: names of measurements and measurement units. Sensor values are
+ *                  displayed separately.
+ *
+ * @return          None
+ */
+void GUI::printBMEpage()
+{
+    // Clear the display before the page is printing
+    lcd->clear();
+
+    // Set the cursor position to the right place on the screen and print the labels for the BME page on the LCD
+    lcd->setCursor(0, 0);
+    lcd->print("Temperature:");
+    lcd->setCursor(19, 0);
+    lcd->print("C");
+
+    lcd->setCursor(0, 1);
+    lcd->print("Humidity:");
+    lcd->setCursor(19, 1);
+    lcd->print("%");
+
+    lcd->setCursor(0, 2);
+    lcd->print("Presure:");
+    lcd->setCursor(17, 2);
+    lcd->print("hPa");
+
+    pageMark(); // Prints the place to insert the page number
+}
+
+/**
+ * @brief           Print content of the CCS page: names of measurements and measurement units. Sensor values are
+ *                  printed separately.
+ *
+ * @return          None.
+ */
+void GUI::printCCSpage()
+{
+    // Clear the display before the page is printing
+    lcd->clear();
+
+    // Set the cursor position to the right place on the screen and print the labels for the CCS page on the LCD
+    lcd->setCursor(0, 0);
+    lcd->print("CO2:");
+    lcd->setCursor(17, 0);
+    lcd->print("ppm");
+
+    lcd->setCursor(0, 1);
+    lcd->print("tVOC:");
+    lcd->setCursor(15, 1);
+    lcd->print("ug/m3");
+
+    pageMark(); // Prints the place to insert the page number
+}
+
+/**
+ * @brief           Print content of the each PMS page: names of measurements and measurement units. Sensor values are
+ *                  printed separately.
+ *
+ * @param page      The number of PMS page we want to display. There are 3 PMS pages (1 - 3).
+ *
+ * @return          None.
+ */
+void GUI::printPMSpage(byte page)
+{
+    // Clear the display before the page is printing
+    lcd->clear();
+
+    // Select the page depending on the page argument
+    // Set the cursor position to the right place on the screen and print the labels for the PMS page on the LCD
+    switch (page)
+    {
+    case 1:
+        lcd->setCursor(0, 0);
+        lcd->print("PM1.0: ");
+        lcd->setCursor(15, 0);
+        lcd->print("ug/m3");
+
+        lcd->setCursor(0, 1);
+        lcd->print("PM2.5: ");
+        lcd->setCursor(15, 1);
+        lcd->print("ug/m3");
+
+        lcd->setCursor(0, 2);
+        lcd->print("PM10: ");
+        lcd->setCursor(15, 2);
+        lcd->print("ug/m3");
+        break;
+
+    case 2:
+        lcd->setCursor(0, 0);
+        lcd->print("N0.3: ");
+        lcd->setCursor(13, 0);
+        lcd->print("#/100cc");
+
+        lcd->setCursor(0, 1);
+        lcd->print("N0.5: ");
+        lcd->setCursor(13, 1);
+        lcd->print("#/100cc");
+
+        lcd->setCursor(0, 2);
+        lcd->print("N1.0: ");
+        lcd->setCursor(13, 2);
+        lcd->print("#/100cc");
+        break;
+
+    case 3:
+        lcd->setCursor(0, 0);
+        lcd->print("N2.5: ");
+        lcd->setCursor(13, 0);
+        lcd->print("#/100cc");
+
+        lcd->setCursor(0, 1);
+        lcd->print("N5.0: ");
+        lcd->setCursor(13, 1);
+        lcd->print("#/100cc");
+
+        lcd->setCursor(0, 2);
+        lcd->print("N10: ");
+        lcd->setCursor(13, 2);
+        lcd->print("#/100cc");
+        break;
+    }
+
+    pageMark(); // Prints the place to insert the page number
+}
+
+/**
+ * @brief           Function to insert only sensor measurements on display for each page.
+ *
+ * @param page      The page on which we want to insert numbers.
+ *
+ * @return          None.
+ */
+void GUI::insertNumbers(byte page)
+{
+    // Variables for bme values
+    float bmeTemp, bmeHumidity, bmePressure, bmeGas, tempOffset = 0;
+
+    if (page > 2) // Those are PMS pages so we need to read the PMS values if the page number is greater than 2
+    {
+        pms->read(); // PMS reads values and stores them in their variables
+    }
+
+    // Select the page depending on the page argument
+    switch (page)
+    {
+    case 1:
+        bme280->readSensorData(bmeTemp, bmeHumidity, bmePressure);       // readBME
+        printBMEvalues(bmeTemp - TEMP_OFFEST, bmeHumidity, bmePressure); // Printing it to the LCD on the right place
+        break;
+
+    case 2:
+        if (ccs811Sensor->dataAvailable()) //  Check to see if data is ready
+        {
+            ccs811Sensor->readAlgorithmResults(); // Read Alorithm only reads data into internal registers of sensor,
+                                                  // doesnt return anything
+            printCCSvalues(ccs811Sensor->getCO2(),
+                           ccs811Sensor->getTVOC()); // Printing it to the LCD on the right place
+        }
+        else
+        {
+            printCCSvalues(-1, -1);
+        }
+        break;
+
+    case 3:
+        if (pms)
+        {
+            // Printing it to the LCD on the right place
+            printPMSvalues(1, pms->pm01, pms->pm25, pms->pm10, pms->n0p3, pms->n0p5, pms->n1p0, pms->n2p5, pms->n5p0,
+                           pms->n10p0);
+        }
+        else
+        {
+            // Set -1 to indicate that there is an error
+            printPMSvalues(1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+        }
+        break;
+
+    case 4:
+        if (pms)
+        {
+            // Printing it to the LCD on the right place
+            printPMSvalues(2, pms->pm01, pms->pm25, pms->pm10, pms->n0p3, pms->n0p5, pms->n1p0, pms->n2p5, pms->n5p0,
+                           pms->n10p0);
+        }
+        else
+        {
+            // Set -1 to indicate that there is an error
+            printPMSvalues(2, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+        }
+        break;
+
+    case 5:
+        if (pms)
+        {
+            // Printing it to the LCD on the right place
+            printPMSvalues(3, pms->pm01, pms->pm25, pms->pm10, pms->n0p3, pms->n0p5, pms->n1p0, pms->n2p5, pms->n5p0,
+                           pms->n10p0);
+        }
+        else
+        {
+            // Set -1 to indicate that there is an error
+            printPMSvalues(3, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+        }
+        break;
+    }
+}
+
+/**
+ * @brief           Print values that BME reads in the right place on the page.
+ *
+ * @param temp      The temperature we want to display.
+ *
+ * @param hum       The humidity we want to display.
+ *
+ * @param pressure  The pressure we want to display.
+ *
+ * @return          None.
+ */
+void GUI::printBMEvalues(float temp, float hum, float pressure)
+{
+    if (!BMEavailable())
+    {
+        // If the sensor is available, print read values and round them to 1 decimal
+        lcd->setCursor(13, 0);
+        lcd->print(temp, 1);
+
+        lcd->setCursor(13, 1);
+        lcd->print(hum, 1);
+
+        lcd->setCursor(9, 2);
+        lcd->print(pressure, 1);
+    }
+    else
+    {
+        // If the sensor isn't available, print Err instead numbers
+        lcd->setCursor(13, 0);
+        lcd->print("Err");
+
+        lcd->setCursor(13, 1);
+        lcd->print("Err");
+
+        lcd->setCursor(9, 2);
+        lcd->print("Err");
+    }
+}
+
+/**
+ * @brief           Print values that CCS read in the right place on the page.
+ *
+ * @param ccsCO2    The CO2 concentration value to display.
+ *
+ * @param ccsTVOC   The TVOC value to display.
+ *
+ * @return          None.
+ */
+void GUI::printCCSvalues(int ccsCO2, int ccsTVOC)
+{
+    if (ccsCO2 == -1)
+    {
+        // If there is any error, print Err instead of values
+        lcd->setCursor(10, 0);
+        lcd->print("Err");
+
+        lcd->setCursor(10, 1);
+        lcd->print("Err");
+    }
+    else
+    {
+        // If not, print the CCS values
+        lcd->setCursor(10, 0);
+        lcd->print(ccsCO2);
+
+        lcd->setCursor(10, 1);
+        lcd->print(ccsTVOC);
+    }
+}
+
+/**
+ * @brief           Prints values that PMS read in the right place on each page.
+ *
+ * @param page      Page of the PMS sensor on which we want to display read values. There are 3 pages, from 1 to 3.
+ *
+ * @param pm1       The concentration of the pm1 particules to display.
+ *
+ * @param pm25       The concentration of the pm25 particules to display.
+ *
+ * @param pm10       The concentration of the pm10 particules to display.
+ *
+ * @param n0p3       The concentration of the n0p3 particules to display.
+ *
+ * @param n0p5       The concentration of the n0p5 particules to display.
+ *
+ * @param n1p0       The concentration of the n1p0 particules to display.
+ *
+ * @param n2p5       The concentration of the n2p5 particules to display.
+ *
+ * @param n5p0       The concentration of the n5p0 particules to display.
+ *
+ * @param n10p0       The concentration of the n10p0 particules to display.
+ *
+ * @return          None.
+ */
+void GUI::printPMSvalues(byte page, int pm1, int pm25, int pm10, int n0p3, int n0p5, int n1p0, int n2p5, int n5p0,
+                         int n10p0)
+{
+    if (pm1 == -1)
+    {
+        // Above we send -1 to this function if there was any error so there print it
+        lcd->clear();
+        lcd->setCursor(0, 0);
+        lcd->print("Error reading sensor");
+    }
+
+    // Select the page depending on the page argument and print the values
+    switch (page)
+    {
+    case 1:
+        lcd->setCursor(10, 0);
+        lcd->print(pm1);
+
+        lcd->setCursor(10, 1);
+        lcd->print(pm25);
+
+        lcd->setCursor(10, 2);
+        lcd->print(pm10);
+        break;
+
+    case 2:
+        lcd->setCursor(7, 0);
+        lcd->print(n0p3);
+
+        lcd->setCursor(7, 1);
+        lcd->print(n0p5);
+
+        lcd->setCursor(7, 2);
+        lcd->print(n1p0);
+        break;
+
+    case 3:
+        lcd->setCursor(8, 0);
+        lcd->print(n2p5);
+
+        lcd->setCursor(8, 1);
+        lcd->print(n5p0);
+
+        lcd->setCursor(8, 2);
+        lcd->print(n10p0);
+        break;
+    }
+}
+
+/**
+ * @brief           Insert page number in the right place.
+ *
+ * @return          None.
+ */
+void GUI::insertPageNum()
+{
+    lcd->setCursor(7, 3);
+    lcd->print(*page + 1);
+}
+
+/**
+ * @brief           Prints the place for the page number in the last row.
+ *
+ * @return          None.
+ */
+void GUI::pageMark()
+{
+    lcd->setCursor(1, 3);
+    lcd->print("Page:  /");
+    lcd->print(*maxPage);
+}
+
+/**
+ * @brief           Initialize LCD and print splash screen.
+ *
+ * @return          None.
+ */
+void GUI::displayBegin()
+{
+    // Initialize LCD and turn on the backlight
+    lcd->begin();
+    lcd->backlight();
+
+    // Set the cursor to the right position and print the splash screen
+    lcd->setCursor(1, 1);
+    lcd->print("Air quality sensor");
+    lcd->setCursor(4, 2);
+    lcd->print("by Soldered");
+}
+
+/**
+ * @brief           Initialize the BME sensor and take measurements.
+ *
+ * @return          None.
+ */
+void GUI::BMEbegin()
+{
+    float bmeTemp, bmeHumidity, bmePressure;
+
+    bme280->begin();
+    bme280->readSensorData(bmeTemp, bmeHumidity, bmePressure);
+}
+
+/**
+ * @brief           Initialize the CCS sensor and take measurements.
+ *
+ * @return          None.
+ */
+void GUI::CCSbegin()
+{
+    if (ccs811Sensor->begin())
+    {
+        if (ccs811Sensor->dataAvailable())
+        {
+            // Read CCS
+            ccs811Sensor->readAlgorithmResults();  // Read Alorithm only reads data into internal registers of sensor,
+                                                   // doesnt return anything
+            int ccsCO2 = ccs811Sensor->getCO2();   // Returns calculated CO2 reading
+            int ccsTVOC = ccs811Sensor->getTVOC(); // Returns calculated TVOC reading
+        }
+    }
+}
+
+/**
+ * @brief           Initialize PMS sensor and take measurements.
+ *
+ * @return          None.
+ */
+void GUI::PMSbegin()
+{
+    pms->begin();
+    pms->read();
+}
+
+/**
+ * @brief           Check if the BME responses on the I2C, if so, it is probably available
+ *
+ * @return          Wire.endTransmission code. 0 means successfully.
+ */
+byte GUI::BMEavailable()
+{
+    Wire.beginTransmission(0x76);
+
+    byte err = Wire.endTransmission();
+    return err;
+}
+
+/**
+ * @brief           Calculate and print battery level in percent.
+ *
+ * @return          None.
+ */
+void GUI::printBatteryPercentage()
+{
+    // Measure and calculate battery level
+    int batteryVoltage = analogRead(BATTERY_MEASURE_PIN);
+    byte batteryPercentage = map(batteryVoltage, BATTERY_LOW, BATTERY_FULL, 0, 100);
+
+    // Print baterry level
+    lcd->setCursor(11, 3);
+    lcd->print("Bat: ");
+    lcd->print(batteryPercentage);
+    lcd->print("%");
+}
